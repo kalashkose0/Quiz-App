b4074d6052bd0f9763d6796e023599f3348a153f
Kalash <kalashkose0@gmail.com>
2025-02-28 19:56:38 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: Home(),
     );
   }
 }
