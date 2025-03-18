346a041b31611b8957a8b4576da4f17cf2a00f11
Kalash <kalashkose0@gmail.com>
2025-03-18 22:16:35 +0530

    z

--- lib/pages/AnimalPage/AnimalQuestion.dart
+++ lib/pages/AnimalPage/AnimalQuestion.dart
@@ -1,136 +1,136 @@
-import 'package:flutter/material.dart';
-import 'package:quiz_app/pages/home.dart';
+  import 'package:flutter/material.dart';
+  import 'package:quiz_app/pages/home.dart';
 
-class Animalquestion extends StatefulWidget {
-  const Animalquestion({super.key});
+  class Animalquestion extends StatefulWidget {
+    const Animalquestion({super.key});
 
-  @override
-  State<Animalquestion> createState() => _AnimalquestionState();
-}
+    @override
+    State<Animalquestion> createState() => _AnimalquestionState();
+  }
 
-class _AnimalquestionState extends State<Animalquestion> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 2, 150, 132),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Home()));
-                  },
-                  child: CircleAvatar(
-                    backgroundColor: Colors.orange,
-                    radius: 20,
-                    child: Icon(
-                      Icons.chevron_left_sharp,
-                      size: 30,
+  class _AnimalquestionState extends State<Animalquestion> {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Color.fromARGB(255, 2, 150, 132),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.orange,
+                      radius: 20,
+                      child: Icon(
+                        Icons.chevron_left_sharp,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 90,
+                  ),
+                  Text(
+                    "Animal",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
                       color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 20, right: 20, top: 40),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              "assets/images/dog.png",
+                              height: 240,
+                              width: MediaQuery.of(context).size.width,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        OptionBox(text: "beer"),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        OptionBox(text: "cat"),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        OptionBox(text: "dog"),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        OptionBox(text: "deer"),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Add navigation or action here
+                          },
+                          child: Text("Next"),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                SizedBox(
-                  width: 90,
-                ),
-                Text(
-                  "Animal",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 20, right: 20, top: 40),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            "assets/images/dog.png",
-                            height: 240,
-                            width: MediaQuery.of(context).size.width,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      OptionBox(text: "beer"),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      OptionBox(text: "cat"),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      OptionBox(text: "dog"),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      OptionBox(text: "deer"),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Add navigation or action here
-                        },
-                        child: Text("Next"),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
 
-class OptionBox extends StatelessWidget {
-  final String text;
-  const OptionBox({Key? key, required this.text}) : super(key: key);
+  class OptionBox extends StatelessWidget {
+    final String text;
+    const OptionBox({Key? key, required this.text}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 2.0),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Text(
-        text,
-        style: TextStyle(
-            fontSize: 22, color: Colors.black, fontWeight: FontWeight.w500),
-      ),
-    );
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 2.0),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+              fontSize: 22, color: Colors.black, fontWeight: FontWeight.w500),
+        ),
+      );
+    }
   }
-}
